9559a2dd34b4e621780f9a103d7a298c322bd8f6
nayu <19748682+nayu7u@users.noreply.github.com>
2024-10-23 16:21:44 +0000

    test: check the added validation

--- spec/factories/feeds.rb
+++ spec/factories/feeds.rb
@@ -2,5 +2,15 @@ FactoryBot.define do
   factory :feed do
     name { "MyString" }
     url { "MyString" }
+
+    after(:build) do |feed|
+      def feed.rss
+        if url == "invalid url"
+          raise
+        else
+          RSS::RDF.new
+        end
+      end
+    end
   end
 end
--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Feed, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  describe "validate :url_is_parseable" do
+    it "is valid if url is parsearble" do
+      expect(build(:feed)).to be_valid
+    end
+
+    it "is invalid if url is not parsearble" do
+      expect(build(:feed, url: "invalid url")).not_to be_valid
+    end
+  end
 end
